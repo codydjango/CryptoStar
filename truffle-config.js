946bc43e631a4674f0b0431d3f3648a5acdaf720
Cody Django Redmond <codydjango@gmail.com>
2019-03-21 15:53:52 -0700

    deployed to rinkelby

--- truffle-config.js
+++ truffle-config.js
@@ -28,6 +28,11 @@
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
 
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const infuraKey = "dd83a2d1c03b43779a52e92b9c52c683";
+const mnemonic = "vicious lunch inspire organ dynamic wife clutch sound reason nose describe wave";
+
+
 module.exports = {
   /**
    * Networks define how you connect to your ethereum client and let you set the
@@ -50,6 +55,13 @@ module.exports = {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
+    },
+
+    rinkeby: {
+      provider: () => new HDWalletProvider(mnemonic, `https://rinkeby.infura.io/v3/${ infuraKey }`),
+        network_id: 4,       // rinkeby's id
+        gas: 4500000,        // rinkeby has a lower block limit than mainnet
+        gasPrice: 10000000000
     },
 
     // Another network with more advanced options...
